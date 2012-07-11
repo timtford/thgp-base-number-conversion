9e0d7556d7bd0293ae401ce0677bfd842bb91952
timothy <timothy@labs.hiddengeniusproject.org>
2012-07-11 14:19:48 -0700

    removed cached files to insure .gitignore is working

--- binary/thgpViewController.h
+++ binary/thgpViewController.h
@@ -9,20 +9,21 @@
 #import <UIKit/UIKit.h>
 
 @interface thgpViewController : UIViewController 
- <UIPickerViewDataSource , UIPickerViewDelegate>
+<UIPickerViewDataSource , UIPickerViewDelegate>
+
 {
     
     UITextField *inputText;
     UITextField *outputText;
     IBOutlet UIPickerView *singlePicker;
     NSArray *pickerData;
+    
 }
 
 @property (nonatomic, retain) IBOutlet UITextField *inputText;
 @property (nonatomic, retain) IBOutlet UITextField *outputText;
-@property(nonatomic , retain) UIPickerView *singlePicker;
+@property (nonatomic, retain) UIPickerView *singlePicker;
 @property(nonatomic , retain) NSArray *pickerData;
-
 
 
 -(IBAction)converter:(id)sender;
